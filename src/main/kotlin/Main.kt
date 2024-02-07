4501b2ee9a9133f4f06fe9157625c2a2bfccd7bb
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-02-07 07:30:39 +0300

    added a file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.security.KeyStore.TrustedCertificateEntry
+
+fun main(args: Array<String>) {
+    var fullName = "Esther Terry"
+    fullName = "Esther Terry"
+    println(fullName)
+    var age = 20
+    age = 20
+    println(age)
+    var phoneNumber = "+254-723-071-567"
+    phoneNumber = "+254-723-071-567"
+    println(phoneNumber)
+    var weight = 54
+    weight = 54
+    println(weight)
+    var isKenyan = "True"
+    isKenyan = "True"
+    println(isKenyan)
+
+
+}
